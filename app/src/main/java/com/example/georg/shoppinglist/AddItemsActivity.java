17ebe7f26433e576d068fb542d95d6074c5fc38d
Ambe Che <tamanji.che@metropolia.fi>
2018-12-05 21:05:09 +0200

    addToList method modified

--- app/src/main/java/com/example/georg/shoppinglist/AddItemsActivity.java
+++ app/src/main/java/com/example/georg/shoppinglist/AddItemsActivity.java
@@ -1,5 +1,6 @@
 package com.example.georg.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.util.Scanner;
+
+import static com.example.georg.shoppinglist.MainActivity.KEY_USER;
 
 public class AddItemsActivity extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public class AddItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_items);
 
         Bundle b= getIntent().getExtras();
-        int i=b.getInt(MainActivity.KEY_USER,0);
+        int i=b.getInt(KEY_USER,0);
     }
     public  void addToList(View view){
         //Scanner reader = new Scanner(System.in);
@@ -26,5 +29,11 @@ public class AddItemsActivity extends AppCompatActivity {
         int d = Integer.parseInt(amount.getText().toString());
         ItemListHub.getInstance().getItems().add(new Item(itemName.getText().toString(),d));
 
+    }
+
+    public void previewList(View view){
+        Intent nextActivity = new Intent( AddItemsActivity.this, ListGeneratorActivity.class);
+        //nextActivity.putExtra(KEY_USER);
+        startActivity(nextActivity);
     }
 }
---
+++ app/src/main/java/com/example/georg/shoppinglist/ListGeneratorActivity.java
@@ -0,0 +1,30 @@
+package com.example.georg.shoppinglist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import  java.util.ArrayList;
+
+public class ListGeneratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_generator);
+
+        ListView lview = (ListView) findViewById(R.id.itemListView);
+
+        lview.setAdapter(new ArrayAdapter<Item>(
+                this,
+                android.R.layout.simple_list_item_1,
+                ItemListHub.getInstance().getItems()));
+
+
+
+    }
+}
